167cd4081012b14e354c3675c2dfb47840c1b275
NeProUser <neprouserru@gmail.com>
2023-11-23 16:24:19 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 5;
+        Integer  numIntelger = 10;
+        String a;
+        String b;
+        String c;
+        String d;
+
+        a = "Heloo";
+        b = "world";
+        c = "5";
+        d = "one";
+
+        System.out.println(a + b);
+        System.out.println("Heloo world");
+        System.out.println(c + d);
+    }
+}
