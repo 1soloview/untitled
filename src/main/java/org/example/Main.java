6f54f7cea4373d9b6b75f9163a4d65b40bb88988
NeProUser <neprouserru@gmail.com>
2023-11-23 16:40:52 +0300

    Add vars to Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,13 +9,15 @@ public class Main {
         String c;
         String d;
 
-        a = "Heloo";
+        a = "Hello";
         b = "world";
         c = "5";
         d = "one";
 
         System.out.println(a + b);
-        System.out.println("Heloo world");
+        System.out.println("Hello world");
         System.out.println(c + d);
+        System.out.println(numIntelger);
+        System.out.println(number);
     }
 }
